0f3f92df3db0fc27f25ba79405124a9801c32359
cnbrkaydemir <canberk.aydemir@yahoo.com>
2024-08-26 11:08:10 +0300

    Delete security related components for renovation

--- backend/src/main/java/com/trackevents/constants/SecurityConstants.java
+++
@@ -1,6 +0,0 @@
-package com.trackevents.constants;
-
-public class SecurityConstants {
-    public static final String JWT_KEY = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
-    public static final String JWT_HEADER = "Authorization";
-}
