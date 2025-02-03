93a478ce966d140c1e1d26a1923b26dc9ec923f5
cnbrkaydemir <canberk.aydemir@yahoo.com>
2025-02-03 20:49:23 +0300

    Implement redis caching.

--- src/main/java/com/trackevents/EventTrackingApplication.java
+++ src/main/java/com/trackevents/EventTrackingApplication.java
@@ -1,13 +1,22 @@
 package com.trackevents;
 
+import com.trackevents.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ComponentScans({ @ComponentScan("com.trackevents.controller"), @ComponentScan("com.trackevents.config")})
+@EnableCaching
+@EnableTransactionManagement
+@EnableWebSecurity
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @EnableJpaRepositories("com.trackevents.repository")
 public class EventTrackingApplication {
 
---
+++ src/main/java/com/trackevents/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.trackevents.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(redisHost);
+        configuration.setPort(redisPort);
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+}
