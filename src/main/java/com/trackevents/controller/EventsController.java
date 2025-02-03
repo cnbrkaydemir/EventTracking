fe60c82681574b6cb7fb4ff3e50b05646e1b5927
cnbrkaydemir <canberk.aydemir@yahoo.com>
2025-02-03 20:37:41 +0300

    Update controller implementations.

--- src/main/java/com/trackevents/controller/EventsController.java
+++ src/main/java/com/trackevents/controller/EventsController.java
@@ -1,236 +1,76 @@
 package com.trackevents.controller;
 
 
-import com.trackevents.model.EventInfo;
+import com.trackevents.dto.EventDto;
+import com.trackevents.dto.ParticipationDto;
 import com.trackevents.model.Events;
-import com.trackevents.model.Users;
+import com.trackevents.dto.UserDto;
 import com.trackevents.service.EventService;
 import com.trackevents.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
+
 
 @RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class EventsController {
 
     private final EventService eventService;
 
-    private final UserService usv;
+    private final UserService userService;
 
-    @Autowired
-    public EventsController(EventService evs, UserService usv) {
-        this.eventService = evs;
-        this.usv = usv;
+
+    @PostMapping("/event")
+    public ResponseEntity<EventDto> createEvent(@RequestBody Events event) {
+
+        return ResponseEntity.ok(eventService.createEvent(event));
     }
 
-    @PostMapping("/createEvent")
-    public Events createEvent(@RequestBody Events event) {
-        Users adminUser = usv.getByEmail(event.getCreated_by().getUserEmail());
-        event.setCreated_by(adminUser);
-        event.getParticipants().add(adminUser);
-        adminUser.getEvents().add(event);
-        eventService.createEvent(event);
-        return event;
+    @GetMapping("/event/{id}")
+    public ResponseEntity<EventDto> findEventById(@PathVariable int id) {
+        return ResponseEntity.ok(eventService.findById(id));
     }
 
-    @PostMapping("/addUser")
-    public Events addUsers(@RequestBody UserDto info) {
-        Events event = eventService.findById(info.getEventId());
-
-        info.getUserIds().forEach((id) -> {
-            Users participant = usv.getById(id);
-            participant.getEvents().add(event);
-            event.getParticipants().add(participant);
-            List<Users> u = event.getParticipants();
-            eventService.createEvent(event);
-        });
-
-        return event;
-    }
-
-    @PostMapping("/discardUser")
-    public Events discardUsers(@RequestBody UserDto info) {
-        Events event = eventService.findById(info.getEventId());
-
-        info.getUserIds().forEach((id) -> {
-            Users participant = usv.getById(id);
-            event.getParticipants().remove(participant);
-            participant.getEvents().remove(event);
-        });
-
-        eventService.createEvent(event);
-
-        return event;
-    }
-
-    @PostMapping("/displayEvents")
-    public List<Events> displayEvents(@RequestBody int id) {
-        eventService.setExpired();
-        Users target = usv.getById(id);
-        List<Events> listWithoutDuplicates = target.getEvents();
-        List<Events> events = listWithoutDuplicates.stream()
-                .distinct()
-                .collect(Collectors.toList());
-
-        if (events != null) {
-            return events;
-        } else {
-            return null;
-        }
-    }
-
-    @GetMapping("/displayAllEvents")
-    public List<Events> displayEvents() {
-        eventService.setExpired();
-        List<Events> events = eventService.getAllEvents();
-
-        if (events != null) {
-            Collections.sort(events, (o1, o2) -> {
-                if (o1.getParticipants().size() > o2.getParticipants().size()) {
-                    return -1;
-                } else if (o1.getParticipants().size() == o2.getParticipants().size()) {
-                    return 0;
-                } else {
-                    return 1;
-                }
-            });
-            while (events.size() != 5) {
-                events.remove(events.size() - 1);
-            }
-
-            return events;
-
-        } else {
-            return null;
-        }
-    }
-
-    @PostMapping("/getParticipationMonth")
-    public List<Integer> getMonth(@RequestBody int id) {
-        AtomicInteger i1 = new AtomicInteger(0);
-        AtomicInteger i2 = new AtomicInteger(0);
-        AtomicInteger i3 = new AtomicInteger();
-        AtomicInteger i4 = new AtomicInteger();
-
-
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new Date());
-        int month = cal.get(Calendar.MONTH);
-
-        Users u = usv.getById(id);
-
-        List<Events> listWithoutDuplicates = u.getEvents();
-        List<Events> events = listWithoutDuplicates.stream()
-                .distinct()
-                .collect(Collectors.toList());
-
-
-        events.forEach((event -> {
-            cal.setTime(event.getEventDate());
-            int startMonth = cal.get(Calendar.MONTH);
-            if (startMonth == month || startMonth == month + 1 || startMonth == month + 2 || startMonth == month + 3) {
-                if (startMonth == month) {
-                    i1.incrementAndGet();
-                } else if (startMonth == month + 1) {
-                    i2.incrementAndGet();
-                } else if (startMonth == month + 2) {
-                    i3.incrementAndGet();
-                } else if (startMonth == month + 3) {
-                    i4.incrementAndGet();
-                }
-            }
-
-
-        }));
-        List<Integer> data = new ArrayList<>();
-        data.add(i1.get());
-        data.add(i2.get());
-        data.add(i3.get());
-        data.add(i4.get());
-
-
-        return data;
-
+    @GetMapping("/event")
+    public ResponseEntity<List<EventDto>> displayAllEvents() {
+        return ResponseEntity.ok(eventService.getAllEvents());
     }
 
 
-    @PostMapping("/displayAbsent")
-    public List<Users> displayAbsent(@RequestBody int id) {
-        Events event = eventService.findById(id);
-        List<Users> users = usv.getAllUsers();
-        List<Users> absents = new ArrayList<>();
-
-        users.forEach((users1 -> {
-            if (!event.getParticipants().contains(users1)) {
-                absents.add(users1);
-            }
-        }));
-
-        return absents;
-
+    @PostMapping("/add")
+    public ResponseEntity<EventDto> addUsersToEvent(@RequestBody ParticipationDto info) {
+        return ResponseEntity.ok(eventService.addUsers(info));
     }
 
-    @PostMapping("/getUpcomingEvents")
-    public List<EventInfo> getUpcoming(@RequestBody int id) {
-        eventService.setExpired();
-        Users target = usv.getById(id);
-
-        List<Events> all = target.getEvents();
-
-        List<Events> upcomings = new ArrayList<>();
-
-        all.forEach(event ->{
-            if(!event.isExpired()){
-                upcomings.add(event);
-            }
-        });
-
-        Collections.sort(upcomings, new Comparator<Events>() {
-            @Override
-            public int compare(Events o1, Events o2) {
-                if (o1.getEventExpired().compareTo(o2.getEventExpired()) > 0) {
-                    return 1;
-                } else if (o1.getEventExpired().compareTo(o2.getEventExpired()) < 0) {
-                    return -1;
-                } else {
-                    return 0;
-                }
-            }
-        });
-    List<Events> upcoming= upcomings.stream()
-        .distinct()
-        .collect(Collectors.toList());
-
-    List<EventInfo> eventInfo= new ArrayList<>();
-
-    upcoming.forEach((event)->{
-        eventInfo.add(new EventInfo(event.getEventTitle(),calculateDifference(new Date(),event.getEventExpired()),event.getEventId(),event.getEventExpired()));
-    });
-
-    if(eventInfo.size()<5){
-        return eventInfo;
-    }
-    else if(eventInfo.size()>5){
-        while(eventInfo.size()!=5){
-            eventInfo.remove(eventInfo.size()-1);
-        }
-        return eventInfo;
+    @PostMapping("/discard")
+    public ResponseEntity<EventDto> discardUsersFromEvent(@RequestBody ParticipationDto info) {
+        return ResponseEntity.ok(eventService.discardUsers(info));
     }
 
-        return eventInfo;
-
-
+    @GetMapping("/attending/{userId}")
+    public ResponseEntity<List<EventDto>> getUserEvents(@PathVariable int userId) {
+        return ResponseEntity.ok(eventService.getUserEvents(userId));
     }
 
-    private long calculateDifference(Date d1,Date d2){
-        long diffInMillies = Math.abs(d1.getTime() - d2.getTime());
-        long diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
 
-        return diff;
+    @GetMapping("/month/{userId}")
+    public ResponseEntity<List<Integer>> getUpcomingEventsByMonth(@PathVariable int userId) {
+        return ResponseEntity.ok(eventService.calculateMonthlyDistribution(userId));
     }
+
+    @GetMapping("/absent/{eventId}")
+    public ResponseEntity<List<UserDto>> displayAbsentUsers(@PathVariable int eventId) {
+        return ResponseEntity.ok(eventService.findAbsent(eventId));
+    }
+
+    @GetMapping("/upcoming/{userId}")
+    public ResponseEntity<List<EventDto>> getUpcomingEventsOfUser(@PathVariable int userId) {
+        return ResponseEntity.ok(eventService.upcomingEvents(userId));
+    }
+
 
 }
--- src/main/java/com/trackevents/controller/UsersController.java
+++ src/main/java/com/trackevents/controller/UsersController.java
@@ -1,108 +1,48 @@
 package com.trackevents.controller;
 
-import com.trackevents.model.Authority;
-import com.trackevents.model.Events;
+
+import com.trackevents.dto.UserDto;
 import com.trackevents.model.Users;
-import com.trackevents.repository.AuthorityRepository;
-import com.trackevents.service.EventService;
 import com.trackevents.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class UsersController {
 
     private final UserService userService;
-    private final EventService evs;
 
 
-    private PasswordEncoder passwordEncoder;
-
-    private AuthorityRepository auth;
-    @Autowired
-    public UsersController(UserService usv, EventService evs, PasswordEncoder pwe, AuthorityRepository auth){
-        this.userService=usv;
-        this.evs=evs;
-        this.passwordEncoder=pwe;
-        this.auth=auth;
+    @PostMapping(path = "/user")
+    public ResponseEntity<UserDto> createUser(@RequestBody Users user){
+        return ResponseEntity.ok(userService.createUser(user));
     }
 
-@PostMapping(path = "/register")
-    public Users signUp(@RequestBody Users user){
-        user.setUserPassword(passwordEncoder.encode(user.getUserPassword()));
-        user.setUserRole("user");
-        Authority userAuthority= new Authority("ROLE_USER",user);
-
-        Set<Authority> userAuthorities= new HashSet<>();
-        userAuthorities.add(userAuthority);
-        user.setAuthorities(userAuthorities);
-        userService.createUser(user);
-         auth.save(userAuthority);
-        return user;
-}
-
-    @PostMapping(path = "/displayUser")
-    public Users displayUser(@RequestBody int id){
-        return userService.getById(id);
+    @GetMapping(path = "/user/{userId}")
+    public ResponseEntity<UserDto> findUserById(@PathVariable int userId){
+        return ResponseEntity.ok(userService.getById(userId));
     }
 
 
-
-@GetMapping(path = "/displayAllUsers")
-    public List<UserParticipation> displayAll(){
-        List<Users> allUsers=userService.getAllUsers();
-        Collections.sort(allUsers, new Comparator<Users>() {
-            @Override
-            public int compare(Users o1, Users o2) {
-                if(o1.getEvents().size()>o2.getEvents().size()){
-                    return -1;
-                }
-                else if(o1.getEvents().size()==o2.getEvents().size()){
-                    return 0;
-                }
-                else{
-                    return 1;
-                }
-            }
-        });
-    while(allUsers.size()!=5){
-        allUsers.remove(allUsers.size()-1);
-    }
-    List<UserParticipation> userInfo=new ArrayList<UserParticipation>();
-
-    allUsers.forEach((users)->{
-        UserParticipation info = new UserParticipation(users.getUserName()+" "+users.getUserSurname(),users.getUserId(),100*users.getEvents().size()/evs.getAllEvents().size() ,users.getEvents().size() );
-        userInfo.add(info);
-    });
-
-        return userInfo;
-}
-
-
-    @PostMapping(path = "/displayEventUsers")
-    public Events displayAll(@RequestBody int id)
-    {
-        return evs.findById(id);
-
-
+    @GetMapping(path = "/user")
+    public ResponseEntity<List<UserDto>> displayAllUsers(){
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 
-    @PostMapping(path = "/grantAdmin")
-    public Users grnatAdmin(@RequestBody int id){
-        Users newAdmin=userService.getById(id);
-        newAdmin.setUserRole("admin");
-        Authority newAuthority= new Authority("ROLE_ADMIN",newAdmin);
-        newAdmin.getAuthorities().add(newAuthority);
-        auth.save(newAuthority);
 
-        return newAdmin;
+    @GetMapping(path = "/participants/{eventId}")
+    public ResponseEntity<List<UserDto>> displayParticipantsOfEvent(@PathVariable int eventId) {
+        return ResponseEntity.ok(userService.getParticipants(eventId));
     }
 
+    @PostMapping(path = "/admin")
+    public ResponseEntity<UserDto> grantAdmin(@RequestBody Users user){
+        return ResponseEntity.ok(userService.createAdmin(user));
+    }
 
 }
