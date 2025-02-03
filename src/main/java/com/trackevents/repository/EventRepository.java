07b5fef7ae887fe21080b49a1ff8e515b1932cab
cnbrkaydemir <canberk.aydemir@yahoo.com>
2025-02-03 20:27:53 +0300

    Update repositories to return optional objects.

--- src/main/java/com/trackevents/repository/EventRepository.java
+++ src/main/java/com/trackevents/repository/EventRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EventRepository extends JpaRepository<Events,Integer> {
 
-    Events findByEventId(int id);
+    Optional<Events> findByEventId(int id);
 
 
 
--- src/main/java/com/trackevents/repository/UserRepository.java
+++ src/main/java/com/trackevents/repository/UserRepository.java
@@ -6,12 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users,Integer> {
-    Users findByUserId(int id);
 
-    List<Users> findByEvents(Events events);
+    Optional<Users> findByUserId(int id);
 
-    List<Users> findByUserEmail(String email);
+    Optional<Users> findByUserEmail(String email);
 }
